29b7fab8d53c0cf52032c43c78c8f9da1570a8d4
oscar <oscar.caasmontes@gmail.com>
2024-12-10 21:48:28 +0100

    completaddo los algoritmos de traducir"

--- fromanosV2.py
+++ fromanosV2.py
@@ -1,18 +1,15 @@
-numeros_romanos = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000
-}
-
 def descomponer(posicion: int, cifra: str) -> int:
     return int(cifra) * 10 ** posicion
 
-def traducir(valor: int) -> str:
-    pass
+def traducir(valor: int) -> str:      
+    simbolos = {
+        1: 'L', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX',
+        10: 'X', 20: 'XX', 30: 'XXX', 40: 'XL', 50: 'L', 60: 'LX', 70: 'LXX', 80: 'LXXX', 90: 'XC',
+        100: 'C', 200: 'CC', 300: 'CCC', 400: 'CD', 500: 'D', 600: 'DC', 700: 'DCC', 800: 'DCCC',
+        900: 'CM', 1000: 'M', 2000: 'MM', 3000: 'MMM'
+    }
+
+    return simbolos[valor]
 
 def a_romanos(numero: int) -> str:
     lista_traducciones = []
--- test_romanosV2.py
+++ test_romanosV2.py
@@ -1,8 +1,12 @@
-from fromanosV2 import a_romanos, descomponer
+from fromanosV2 import a_romanos, descomponer, traducir
 
-def test_sencillo():
+def _test_simbolos_sencillos():
     assert a_romanos(1) == 'I'
     assert a_romanos(500) == 'D'
+
+def _test_doble_repeticion():
+    assert a_romanos(2) == 'II'    
+    assert a_romanos(200) == 'CC'
 
 def test_descomponer():
     resultado = descomponer(0,'9')
@@ -12,5 +16,17 @@ def test_descomponer():
     resultado = descomponer(2,'9')
     assert resultado == 900        
     resultado = descomponer(3,'1')
-    assert resultado == 1000        
+    assert resultado == 1000
+
+def test_traducir():
+    assert traducir(9) == 'IX'
+    assert traducir(30) == 'XXX'
+    assert traducir(900) == 'CM'
+    assert traducir(1000) == 'M'
+    assert traducir(2) == 'II'
+
+    assert traducir(800) == 'DCCC'
+
+def test_pasar_a_romano():
+    assert a_romanos(1939) == 'MCMXXXIX'    
 
